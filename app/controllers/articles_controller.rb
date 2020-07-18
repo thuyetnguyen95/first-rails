c9a6f25df44fe45b97a197797dabe8d3749b59c4
thuyetnv <thuyettiensinh@gmail.com>
2020-07-18 23:10:11 +0700

    Feature: add comment to the article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,6 +36,12 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+
     # Private function
     private
         def articleParams
